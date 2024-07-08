5ca8e67982df1cf0831b93889a340be5522549af
CoblinFrog <coblinfrog@gmail.com>
2024-07-08 01:20:35 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import subprocess
+import os
+from tkinter import *
+from tkinter import ttk
+
+#UI Elements
+root = Tk()
+frame = ttk.Frame(root, padding=5)
+frame.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+crashFiles = BooleanVar(root, False)
+hardwareInfo = BooleanVar(root, False)
+osInfo = BooleanVar(root, False)
+driverInfo = BooleanVar(root, False)
+usbDevices = BooleanVar(root, False)
+runChecks = BooleanVar(root, False)
+
+root.geometry("600x400")
+root.title("System Information Tool")
+
+label1 = ttk.Label(root, text="Please choose below what you would like to be packaged into the .zip file:")
+option1 = ttk.Checkbutton(root, text="Minidump/Crash Files", variable=crashFiles, onvalue=True, offvalue=False)
+
+root.mainloop()
