a57fd7fc1e4ffbedbe9992e1c137a4a70543859e
CoblinFrog <coblinfrog@gmail.com>
2024-07-10 00:38:28 -0700

    added radio buttons for name formatting, fixed minidump function

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ hardwareInfo = BooleanVar(root, False)
 osInfo = BooleanVar(root, False)
 driverInfo = BooleanVar(root, False)
 usbDevices = BooleanVar(root, False)
+nameFormat = StringVar(root)
 
 root.title("System Information Tool")
 
@@ -38,13 +39,17 @@ def miniDump():
     elif os.path.exists(rf"C:\Users\OneDrive\{user}\Downloads"):
         downloadsPath = rf"C:\Users\OneDrive\{user}\Downloads"
     else:
-        print("Else")
         raise downloadNotFound
+
+    if os.path.exists(r"C:\Windows\Minidump"):
+        pass
+    else:
+        raise noMiniDump
 
     try:
         shutil.copytree(r"C:\Windows\Minidump", rf"{downloadsPath}\test\Minidumps")
     except:
-        raise noMiniDump
+        print("Already created")
 
 def runFunction():
     print("hello")
@@ -62,17 +67,23 @@ option5 = ttk.Checkbutton(frame, text="Connected USB Devices", variable=usbDevic
 
 label2 = ttk.Label(frame, text="Please choose how you would like the .zip file's name to be formatted:")
 label2.config(font=("TkDefaultFont", 11))
+format1 = ttk.Radiobutton(frame, text="Simple", variable=nameFormat, value="simple")
+format2 = ttk.Radiobutton(frame, text="Normal", variable=nameFormat, value="normal")
+format3 = ttk.Radiobutton(frame, text="Detailed", variable=nameFormat, value="detailed")
 
 runButton = ttk.Button(frame, text="Start", command=runFunction)
 infoButton = ttk.Button(frame, text="Help")
 
-label1.grid(sticky=W, pady=(0, 10))
+label1.grid(sticky=W, pady=(0,5))
 option1.grid(sticky=W)
 option2.grid(sticky=W)
 option3.grid(sticky=W)
 option4.grid(sticky=W)
 option5.grid(sticky=W)
-label2.grid(sticky=W, pady=10)
-runButton.grid(sticky=E, row=8, pady=(10, 0))
-infoButton.grid(sticky=E, row=8, column=1, pady=(10, 0))
+label2.grid(sticky=W, pady=(10,5))
+format1.grid(sticky=W)
+format2.grid(sticky=W)
+format3.grid(sticky=W)
+runButton.grid(sticky=E, row=11, pady=(10,0))
+infoButton.grid(sticky=E, row=11, column=1, pady=(10,0))
 root.mainloop()
